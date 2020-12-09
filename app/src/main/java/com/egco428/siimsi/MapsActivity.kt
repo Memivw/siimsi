731b3ced1c85e845bec53c6b42731f72c809041a
Unknown <prapapornyingyongkit@gmail.com>
2020-12-10 02:54:16 +0700

    searchview in gg maps

--- app/src/main/java/com/egco428/siimsi/MapsActivity.kt
+++ app/src/main/java/com/egco428/siimsi/MapsActivity.kt
@@ -1,11 +1,17 @@
 package com.egco428.siimsi
 
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.location.places.ui.PlacePicker
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,19 +19,47 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_maps.*
+import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private val LOCATION_PERMISSION_REQUEST = 1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private fun getLocationAccess(){
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            mMap.isMyLocationEnabled = true
+        } else
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == LOCATION_PERMISSION_REQUEST){
+            if(grantResults.contains(PackageManager.PERMISSION_GRANTED)){
+                mMap.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(this,"User has not granted location acccess permission",Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
     }
 
     /**
@@ -39,10 +73,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
+        getLocationAccess()
         // Add a marker in Sydney and move the camera
-        val watjadee = LatLng(8.9112, 99.8475)
-        mMap.addMarker(MarkerOptions().position(watjadee).title("Wat Jadee"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(watjadee))
+//        val watjadee = LatLng(8.9112, 99.8475)
+//        mMap.addMarker(MarkerOptions().position(watjadee).title("Wat Jadee"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(watjadee))
     }
 }
